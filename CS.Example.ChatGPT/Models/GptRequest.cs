66fa66f790caf94e8903e21153be7795e59cfaf9
Giovanni Martínez <gmtz_itv@hotmail.com>
2023-06-12 16:31:36 -0600

    Se agrega método para Crear ChatCompletion (GPT)

---
+++ CS.Example.ChatGPT/Models/GptRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace CS.Example.ChatGPT.Models
+{
+    public class GptRequest
+    {
+        public string model { get { return "gpt-3.5-turbo";  } }
+        public List<Message>? messages { get; set; }
+    }
+}
---
+++ CS.Example.Services/GptService.cs
@@ -0,0 +1,36 @@
+﻿using CS.Example.ChatGPT.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS.Example.Services
+{
+    /// <summary>
+    /// Servicios de chat GTP
+    /// </summary>
+    public static class GptService
+    {
+        /// <summary>
+        /// Método que crea un chat de GPT a través de una petición POST
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        public static async Task<GptResponse> CreateChatCompletion(GptRequest request)
+        {
+            string urlRequest = $"https://openai80.p.rapidapi.com/chat/completions";
+
+            var headers = new Dictionary<string, string>() {
+                { "Content-Type", ContentType.JSON },
+                { "X-RapidAPI-Key", "7bb42da06fmshffec60946a92f3cp1fa214jsn982a8315f1f9"},
+                { "X-RapidAPI-Host", "openai80.p.rapidapi.com"}
+            };
+
+            GptResponse response = await HttpClient.JsonRequest<GptResponse, GptRequest>(request, urlRequest, ContentType.JSON, HttpMethods.Post, headers);
+
+            return response;
+        }
+    }
+}
